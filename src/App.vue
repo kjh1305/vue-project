b99bb9858e41935bca476afd0a8bb22c4725f936
kjh1305 <cosyq1305@naver.com>
2021-08-09 03:36:45 +0900

    vuetify header 수정중

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <Header/>
-    </div>
-    <router-view />
-    <div id="footer">
-      <Footer/>
-    </div>
-  </div>
+  <v-app>
+    <nav>
+      <v-toolbar
+          color="yellow"
+          flat>
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click="sidebar = !sidebar"></v-app-bar-nav-icon>
+        <v-navigation-drawer v-model="sidebar" app hide-overlay>
+          <v-list>
+            <v-list-item v-for="(item, i) in menuItems" exact :key="i" :to="item.path">{{ item.title }}</v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-toolbar-title>
+        <span class="font-weight-light">
+          <router-link to="/">회원 관리 프로그램</router-link>
+        </span>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn>logout
+        </v-btn>
+        <v-btn>
+          <router-link to="/login">Login</router-link>
+        </v-btn>
+
+      </v-toolbar>
+    </nav>
+    <v-main>
+      <router-view/>
+    </v-main>
+    <v-footer>
+
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 
 export default {
-  components: {
-    Header,
-    Footer
-  }
-}
+  name: 'App',
+  data: () => ({
+    sidebar: false,
+    menuItems: [
+      {path: "/", name: "home", title: "Home"},
+      {path: "/users", name: "users", title: "Users"},
+    ]
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  fixed : top;
-  padding-bottom: 50px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #737373;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-</style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from "axios";
-import BootstrapVue from "bootstrap-vue";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueCookies from "vue-cookies"
+import vuetify from './plugins/vuetify'
 
-Vue.use(BootstrapVue)
 Vue.use(VueCookies)
 Vue.config.productionTip = false
 axios.defaults.baseURL = "http://localhost:8888"
@@ -18,5 +15,6 @@ Vue.$cookies.config("1d");
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')

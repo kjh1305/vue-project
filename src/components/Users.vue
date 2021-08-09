67645533988e515d2a62ff70588b485f4b3f4531
kjh1305 <cosyq1305@naver.com>
2021-08-10 04:50:51 +0900

    Users vuex page 구현 아직 안댐

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,76 +1,43 @@
+
 <template>
   <div>
     <v-container>
-    <h1 class="display-2 font-weight-bold text-center ma-10 pa-4">사용자 정보 리스트</h1>
+      <h1 class="display-2 font-weight-bold text-center ma-10 pa-4">사용자 정보 리스트</h1>
       <v-data-table
-          :headers="headers"
+          :headers="userListHeaders"
           :items="userList"
           class="elevation-10"
           :page.sync="page"
-          :items-per-page="pageList.totalPageCnt"
+          :items-per-page="userListPageList.totalPageCnt"
           hide-default-footer
       ></v-data-table>
       <div class="text-center pt-2">
-        <v-pagination v-model="page" :length="pageList.totalPageCnt" class="ma-5"></v-pagination>
+        <v-pagination v-model="page" :length="userListPageList.totalPageCnt" class="ma-5"></v-pagination>
       </div>
     </v-container>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex"
 
 export default {
   name: "Users",
   data: () => {
     return {
-      headers: [
-        { text: 'Id', value: 'id' },
-        { text: 'email', value: 'email' },
-        { text: 'password', value: 'password' },
-        {
-          text: 'name',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'det', value: 'det' },
-      ],
-      userList: [],
-      page:1,
-      perPage:'',
-      pageList:[]
+      page:1
     }
+  },
+  computed:{
+    ...mapState(["userListHeaders","userList","userListPageList"])
   },
   created() {
-    this.$axios.get('/users?page='+this.page)
-        .then(res => {
-          this.userList = res.data
-        })
-        .catch(error => {
-          console.log(error)
-        }),
-    this.$axios.get('/users/page?page='+ this.page)
-        .then(res => {
-          this.pageList = res.data
-        })
-        .catch(error => {
-          console.log(error)
-        })
-  },
-  methods:{
-    pageClick(pageNum){
-      this.$axios.get('/users?page='+pageNum)
-        .then(res =>{
-          this.userList = res.data
-        })
-      .catch(error => {
-        console.log(error)
-      })
-    }
+    this.$store.dispatch("userGetList")
+    this.$store.dispatch("userGetPage")
   },
   watch: {
     page: function (pageNum) {
-      this.pageClick(pageNum);
+      this.$store.dispatch("userPageClick", pageNum)
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import VueCookies from "vue-cookies";
 import router from "../router/index"
+import axios from "axios";
 
 Vue.use(VueCookies)
 Vue.use(Vuex)
@@ -14,9 +15,27 @@ export default new Vuex.Store({
     items: [
       {path: "/", name: "home", title: "Home"},
       {path: "/users", name: "users", title: "Users"},
-    ]
+    ],
+    userListHeaders: [
+      { text: 'Id', value: 'id' },
+      { text: 'email', value: 'email' },
+      { text: 'password', value: 'password' },
+      { text: 'name', value: 'name'},
+      { text: 'det', value: 'det' },
+    ],
+    userList: [],
+    userListPageList:[]
   },
   mutations: {
+    user_List(state, list){
+      state.userList = list
+    },
+    user_Page(state, page){
+      state.userListPageList = page
+    },
+    user_Page_Click(state, list){
+      state.userList = list
+    },
     login_Token(state){
       state.CokesToken = true
     },
@@ -25,6 +44,33 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    userGetList(context){
+      axios.get('/users?page=1')
+          .then(res => {
+            context.commit('user_List', res.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    userGetPage(context){
+      axios.get('/users/page?page=1')
+          .then(res => {
+            context.commit('user_Page', res.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    userPageClick(context, pageNum){
+      axios.get('/users?page='+pageNum)
+          .then(res =>{
+            context.commit('user_Page_Click', res.data, pageNum)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
     login(context){
       context.commit('login_Token')
     },
